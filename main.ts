412539ca678acd74888b5c56e83510d3bc0dce83
simon-bursa <146720121+simon-bursa@users.noreply.github.com>
2024-01-15 13:35:10 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,33 @@
 
+let freq: number = 50
+
+radio.setGroup(69)
+radio.setTransmitSerialNumber(true)
+radio.setTransmitPower(7)
+//radio.setFrequencyBand(69)
+
+function randomString(l: number) { 
+    let letters: string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let ret: string = ""
+
+    for (let i = 0; i <= l; i++) { 
+        ret = ret + letters[randint(0, letters.length - 1)]
+    }
+    return ret
+}
+
+
+
+
+basic.forever(function() {
+    radio.sendValue("a", control.deviceSerialNumber())
+    basic.pause(100)
+})
+
+radio.onReceivedValue(function(name: string, value: number) {
+    if (radio.receivedPacket(RadioPacketProperty.SerialNumber) === -1650614892) {
+        serial.writeValue(name, value)
+     }  
+})
+
+
